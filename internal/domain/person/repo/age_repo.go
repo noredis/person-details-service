f8f657950837fd0fad90b16458cd9c66ccfaf746
Rustam Urazov <rustpy2703@gmail.com>
2025-05-01 06:47:02 +0600

    age repository

---
+++ internal/domain/person/repo/age_repo.go
@@ -0,0 +1,20 @@
+package person_repo
+
+import (
+	"context"
+	"person-details-service/internal/domain/person"
+	vo "person-details-service/internal/domain/person/valueobject"
+)
+
+type AgeRepository interface {
+	FindOutPersonsAge(ctx context.Context, p person.Person) (*vo.Age, error)
+}
+
+type FakeAgeRepository struct{}
+
+func (r FakeAgeRepository) FindOutPersonsAge(ctx context.Context, p person.Person) (*vo.Age, error) {
+	if p.FullName() == "John Doe" {
+		return vo.NewAge(37)
+	}
+	return nil, nil
+}
---
+++ internal/domain/person/repo/age_repo_test.go
@@ -0,0 +1,45 @@
+package person_repo_test
+
+import (
+	"context"
+	"person-details-service/internal/domain/person"
+	repo "person-details-service/internal/domain/person/repo"
+	vo "person-details-service/internal/domain/person/valueobject"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestAgeRepo(t *testing.T) {
+	Convey("Test age repo", t, func() {
+		repo := repo.FakeAgeRepository{}
+
+		Convey("Get age", func() {
+			ctx := context.Background()
+
+			name, _ := vo.NewName("John")
+			surname, _ := vo.NewName("Doe")
+			patronymic := vo.NewPatronymic("")
+			johnDoe := person.CreatePerson(*name, *surname, patronymic)
+
+			age, err := repo.FindOutPersonsAge(ctx, *johnDoe)
+
+			So(age, ShouldNotBeNil)
+			So(err, ShouldBeNil)
+		})
+
+		Convey("Get nil age", func() {
+			ctx := context.Background()
+
+			name, _ := vo.NewName("John")
+			surname, _ := vo.NewName("Doe Jr")
+			patronymic := vo.NewPatronymic("")
+			johnDoe := person.CreatePerson(*name, *surname, patronymic)
+
+			age, err := repo.FindOutPersonsAge(ctx, *johnDoe)
+
+			So(age, ShouldBeNil)
+			So(err, ShouldBeNil)
+		})
+	})
+}
